4aa4fec8708d9de8b880aede866fb89167c7da48
Justine <jjtroyke@gmail.com>
2018-07-25 09:45:25 -0600

    Created route and action in controller, added flash in application.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#index"
+
+  resources :welcome, only: [] do
+    get '/sentences', to: 'welcome#sentences', on: :collection
+    post '/sentences', to: 'welcome#sentences', on: :collection
+  end
 end
