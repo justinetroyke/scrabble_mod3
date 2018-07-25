c521c0644b222ee2cd989670314d1e568d44b1e5
Justine <jjtroyke@gmail.com>
2018-07-25 10:25:10 -0600

    added client id and key

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,9 @@ class WelcomeController < ApplicationController
 
   def sentences
     word = params["word"]
+
+    response = Faraday.get('https://od-api.oxforddictionaries.com/api/v1/entries/en/mindfulness/sentences')
+    binding.pry
     flash[:notice] = "Examples for using '#{word}'"
     redirect_to root_path
   end
--- spec/features/guest_sees_example_sentences_for_word_spec.rb
+++ spec/features/guest_sees_example_sentences_for_word_spec.rb
@@ -6,6 +6,7 @@ describe 'enter word and receive example sentences' do
     it 'submit word and see example senteces with word submitted' do
       word = 'mindfulness'
       header = "Examples for using 'mindfulness'"
+      sentence = "So mindfulness of drinking is already one kind of enlightenment."
 
       visit '/'
       # When I visit "/"
@@ -13,10 +14,9 @@ describe 'enter word and receive example sentences' do
       # And I fill in a text box with "mindfulness"
       click_on 'Submit'
       # And I click "Submit"
-save_and_open_page
       expect(page).to have_content(header)
       # Then I should see a message that says "Examples for using 'mindfulness'"
-      # expect(page).to have_content()
+      expect(page).to have_content(sentence)
       # And I should see a list of sentences with examples of how to use the word
       # And I should see only sentences for usage in North America
       # And I should not see sentences for any other regions
